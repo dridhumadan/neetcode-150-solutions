6a6d227ea286016ae92a68b9b4e2c14e7ccbc45a
Dridhu Madan <dridhumadan@gmail.com>
2022-07-05 19:38:43 +0530

    Intial Commit

---
+++ Tries/208-Implement-Trie.java
@@ -0,0 +1,76 @@
+class TrieNode {
+    TrieNode[] children = new TrieNode[26];
+    boolean isEndOfWord;
+
+    TrieNode() {
+        isEndOfWord = false;
+        for (int i = 0; i < 26; i++) {
+            children[i] = null;
+        }
+    }
+}
+
+class Trie {
+
+    TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode temp = root;
+
+        for (int i = 0; i < word.length(); i++) {
+            int index = word.charAt(i) - 'a';
+
+            if (temp.children[index] == null) {
+                temp.children[index] = new TrieNode();
+            }
+
+            temp = temp.children[index];
+        }
+
+        temp.isEndOfWord = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode temp = root;
+
+        for (int i = 0; i < word.length(); i++) {
+            int index = word.charAt(i) - 'a';
+
+            if (temp.children[index] == null) {
+                return false;
+            }
+
+            temp = temp.children[index];
+        }
+
+        return temp.isEndOfWord;
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode temp = root;
+
+        for (int i = 0; i < prefix.length(); i++) {
+            int index = prefix.charAt(i) - 'a';
+
+            if (temp.children[index] == null) {
+                return false;
+            }
+
+            temp = temp.children[index];
+        }
+
+        return true;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
