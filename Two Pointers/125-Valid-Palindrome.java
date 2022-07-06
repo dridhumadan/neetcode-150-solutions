ec2f982b35782ed0655a99028240f29679bd7489
Dridhu Madan <dridhumadan@gmail.com>
2022-07-06 18:59:55 +0530

    Day 2 - 125-Valid-Palindrome

---
+++ Two Pointers/125-Valid-Palindrome.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+
+        for (int i = 0, j = s.length() - 1; i < j;) {
+            if (!Character.isLetterOrDigit(s.charAt(i))) {
+                i++;
+            } else if (!Character.isLetterOrDigit(s.charAt(j))) {
+                j--;
+            } else if (s.charAt(i++) != s.charAt(j--)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
