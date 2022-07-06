2bd7bbdc30ce78c503b5812b8aa78e4198ea4a4e
Dridhu Madan <dridhumadan@gmail.com>
2022-07-06 20:58:54 +0530

    Day 1 - add comment to 704-Binary-Search

--- Binary Search/704-Binary-Search.java
+++ Binary Search/704-Binary-Search.java
@@ -1,3 +1,12 @@
+/*
+ * Maintain three pointers low -> 0, mid -> (high + low) / 2,  and high -> (n - 1).
+ * Calculate mid. If number at mid index is the target, number is found.
+ * If number at mid index is greater than the target, halve the search space by moving high to mid - 1.
+ * If number at mid index is lesser than the target, halve the search space by moving low to mid + 1.
+ *
+ * Time complexity - O(log n)
+ * Space complexity - O(1)
+ */
 class Solution {
     public int search(int[] nums, int target) {
         int low = 0, high = nums.length - 1, mid = 0;
