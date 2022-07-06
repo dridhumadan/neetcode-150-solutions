3e3caada5585cf210ed23fe20f0c17d7126c612d
Dridhu Madan <dridhumadan@gmail.com>
2022-07-06 21:02:30 +0530

    Day 2 - add comment to 20-Valid-Parentheses

--- Stack/20-Valid-Parentheses.java
+++ Stack/20-Valid-Parentheses.java
@@ -1,3 +1,12 @@
+/*
+ * Create a stack.
+ * Iterate through the provided expression, checking each character.
+ * Add corresponding closing bracket to the stack if an opening bracket is found.
+ * If current character is a closing bracket, check if stack is empty or if top element in the stack is a matching bracket.
+ *
+ * Time complexity - O(n)
+ * Space complexity - O(n)
+ */
 class Solution {
     public boolean isValid(String s) {
         if (s.length() % 2 != 0) {
@@ -16,7 +25,6 @@ class Solution {
             } else if (stack.empty() || stack.pop() != c) {
                 return false;
             }
-            ;
         }
 
         return stack.empty();
