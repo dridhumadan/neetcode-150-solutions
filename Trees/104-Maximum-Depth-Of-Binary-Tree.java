b3551914d3c77dbda6e1f0867787625b998b4767
Dridhu Madan <dridhumadan@gmail.com>
2022-07-08 19:52:42 +0530

    add 2nd solution to 104-Maximum-Depth-Of-Binary-Tree

--- Trees/104-Maximum-Depth-Of-Binary-Tree.java
+++ Trees/104-Maximum-Depth-Of-Binary-Tree.java
@@ -19,14 +19,14 @@ class TreeNode {
 
 /*
  * Recursively check depth of left and right subtree starting with the root
- * node.
+ * node - DFS
  * At each level, add one to the max depth of either left or right subtree and
  * return.
  *
  * Time complexity - O(n)
  * Space complexity - O(h) -> for call stack, where h is the height of the tree
  */
-class Solution {
+class Solution1 {
     public int maxDepth(TreeNode root) {
         if (root == null) {
             return 0;
@@ -36,5 +36,43 @@ class Solution {
         int rightDepth = maxDepth(root.right);
 
         return 1 + Math.max(leftDepth, rightDepth);
+    }
+}
+
+/*
+ * Peform a BFS of the tree
+ * At each level, increment the depth
+ *
+ * Time complexity - O(n)
+ * Space complexity - O(n)
+ */
+class Solution2 {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int depth = 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            depth++;
+
+            for (int i = 0; i < size; i++) {
+                TreeNode temp = queue.poll();
+
+                if (temp.left != null) {
+                    queue.offer(temp.left);
+                }
+
+                if (temp.right != null) {
+                    queue.offer(temp.right);
+                }
+            }
+        }
+
+        return depth;
     }
 }
